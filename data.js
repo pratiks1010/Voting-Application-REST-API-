c3fea1241ae9ec0cce0e8edbdf87684afa83682f
pratiks1010 <pratik10008sawant@gmail.com>
2024-05-08 18:20:01 +0530

    Commit message

---
+++ data.js
@@ -0,0 +1,59 @@
+
+
+[
+    {
+        "name": "pratik sawant",
+        "age": 25,
+        "email": "pratik@gmail.com",
+        "mobile": "7020095352",
+        "address": "242, Rohini Nager 3 Jule Solapur",
+        "aadharCardNumber": 123456789123,
+        "password": "admin123",
+        "role": "admin"
+        },
+
+
+        {
+            "name": "rupesh shinde",
+            "age": 25,
+            "email": "rupesh@gmail.com",
+            "mobile": "7020095352",
+            "address": "242, Rohini Nager 3 Jule Solapur",
+            "aadharCardNumber": 123456789145,
+            "password": "rupesh123",
+            "role": "voter"
+            },
+
+
+            {
+                "name": "rutik ghotane",
+                "age": 25,
+                "email": "rutik@gmail.com",
+                "mobile": "9865941235",
+                "address": "242, Rohini Nager 3 Jule Solapur",
+                "aadharCardNumber": 123456789134,
+                "password": "admin123",
+                "role": "admin"
+                },
+
+
+    
+
+],
+[
+    {
+        "name" : "Narendra Modi",
+        "party" : "BJP",
+        "age" : 73
+    },
+    {
+        "name" : "Rahul gandhi ",
+        "party" : "INC",
+        "age" : 53
+    }
+]
+
+
+
+
+
---
+++ models/candidate.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const User = require('./user');
+const candidateSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    party: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    voters: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+                required: true
+            },
+            votedAt: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    voteCount: {
+        type: Number,
+        default: 0
+    }
+});
+
+const Candidate = mongoose.model('Candidate', candidateSchema);
+
+module.exports = Candidate;
